7467b80e88efb1943683128c3a7e12f219b410c4
AsH3-git <a.shulga2@g.nsu.ru>
2026-05-29 20:19:18 +0700

    Second attempt to generate particle ellips using Twiss params. Added report.typ and compiled pdf document as alternative to README file.

--- colimation.py
+++ colimation.py
@@ -51,63 +51,61 @@ def pdf(r, phi, phi0, r0, sx=0.5, sy=1):
 sx = np.hypot(tex0, x0/L)
 sy = np.hypot(tey0, y0/L)
 
-"""
-def beam_generator(epsilon):
-    gfKEDR = gamma_func_KEDR(beta_func_KEDR)
-    r = sqrt(epsilon / gfKEDR)
-    phi = sqrt(epsilon / beta_func_KEDR)
-    phi0 = 0 #phi0 = -r * alpha/beta but alpha_KEDR == 0
-    r0 = 0 #r0 = -phi * alpha/gamma, alpha_KEDR == 0
-
-    distribution = pdf(r, phi, phi0, r0)
-    return 0
-"""
 def beam_generator(alpha_func, beta_func, epsilon):
     """Function that generates beam distribution using np.random and other numpy tools. Should be equals to the pdf"""
     gf = gamma_func(beta_func)
     coord_disp = sqrt(epsilon * beta_func)
     x = np.arange(-coord_disp, coord_disp, 0.5)
-    x0_hatch = -x[::-1] * alpha_func/beta_func
+    x0_hatch = -x * alpha_func/beta_func
+    x0 = -x * alpha_func/gf
     rng = np.random.default_rng()
-    """
-    epsilon_disp1 = np.linspace(-sqrt(epsilon * gf), sqrt(epsilon gfKEDR = gamma_func_KEDR(beta_func_KEDR)
-    r = sqrt(epsilon / gfKEDR)
-    phi = sqrt(epsilon / beta_func_KEDR)
-    phi0 = 0 #phi0 = -r * alpha/beta but alpha_KEDR == 0
-    r0 = 0 #r0 = -phi * alpha/gamma, alpha_KEDR == 0
-/ beta_func), len(x)//2)
-    epsilon_disp2 = np.linspace(sqrt(epsilon / beta_func), sqrt(epsilon * gf), (len(x)//2 + 1))
-    epsilon_disp = np.concatenate((epsilon_disp1, epsilon_disp2))
-    """
-    epsilon_disp1 = np.linspace(-sqrt(epsilon * gf), sqrt(epsilon / beta_func), len(x)//2)
-    epsilon_disp2 = np.linspace(sqrt(epsilon / beta_func), 0, (len(x)//2 + 1))
-    epsilon_disp = np.concatenate((epsilon_disp2, epsilon_disp1))
-    """
-    for i in range(len(x0_hatch)):
-        radius = epsilon_disp[i]
-        if (x0_hatch[i] < 0) and (radius < 0):
-            angle_dot = rng.uniform(low=abs(x0_hatch[i] - radius), high=abs(x0_hatch[i] + radius), size=100)
-        elif ((x0_hatch[i] < 0) and (radius > 0)):
-            angle_dot = rng.uniform(low=abs(x0_hatch[i] + radius), high=abs(x0_hatch[i] - radius), size=100)
-        elif (x0_hatch[i] > 0) and (radius < 0):
-            angle_dot = rng.uniform(low=abs(x0_hatch[i] + radius), high=abs(x0_hatch[i] - radius), size=100)
-        elif (x0_hatch[i] > 0) and (radius > 0):
-            angle_dot = rng.uniform(low=abs(x0_hatch[i] - radius), high=abs(x0_hatch[i] + radius), size=100)
-        if i == 0:
-            total_distr = angle_dot
-        else:
-            total_distr = np.concatenate((total_distr, angle_dot))
-    """
-#    np.savetxt("2d_distr.txt", total_distr)
+    weights_arr = np.array([])
+    total_distr_angle = np.array([])
+    total_distr_coord = np.array([])
+    angle_radius = sqrt(epsilon / beta_func)
+    coord_radius = sqrt(epsilon / gf)
+    for i in range(len(x)):
+        j = abs(i - len(x)/2)
+        weights_arr = np.append(weights_arr, j)
+        low_angle = min(x0_hatch[i] - angle_radius, x0_hatch[i] + angle_radius)
+        high_angle = max(x0_hatch[i] - angle_radius, x0_hatch[i] + angle_radius)
+        angle_dot = rng.uniform(low=low_angle, high=high_angle, size=100)
+        low_coord = min(x0[i] - coord_radius, x0[i] + coord_radius)
+        high_coord = max(x0[i] - coord_radius, x0[i] + coord_radius)
+        coord_dot = rng.uniform(low=low_coord, high=high_coord, size=100)
+        total_distr_angle = np.concatenate((total_distr_angle, angle_dot))
+        total_distr_coord = np.concatenate((total_distr_coord, coord_dot))
 
+    j_repl_idx = np.where(weights_arr == 0)
+    weights_arr[j_repl_idx] = 1
+    for i, element in enumerate(weights_arr):
+        if element == 1:
+            continue
+        weights_arr[i] = abs(element - (max(weights_arr) - 1)) / max(weights_arr)
+    #print(weights_arr)
+    #weights_arr1 = weights_arr[:len(weights_arr)//2]
+    #weights_arr2 = weights_arr[len(weights_arr)//2:]
+    #weights_arr = np.concatenate((weights_arr1, weights_arr2))
+    
     """
-    zero_level = phi0
-    radius = 15
-    low_limit = phi0 - radius
-    high_limit = phi0 + radius
-    data_distr_2d = rng.uniform(low=low_limit,high=high_limit, size=(1000, 2))
-    np.savetxt("test_2d.txt", data_distr_2d)
+    total_distr_angle = total_distr_angle * weights_arr
+    print(total_distr_angle)
+    total_distr_coord = total_distr_coord * weights_arr
+    
+    angle_diff = x0_hatch - angle_radius
+    coord_diff = x0 - coord_radius
+    angle_diff = np.repeat(angle_diff, 100)
+    coord_diff = np.repeat(coord_diff, 100)
+    weights_arr = np.repeat(weights_arr, 100)
+    mask_angle = (element < (angle_diff * weights_arr))
+    mask_coord = (element < (coord_diff * weights_arr))
+
+    total_distr_angle = total_distr_angle[mask_angle]
+    total_distr_coord = total_distr_coord[mask_coord]
     """
+    
+    np.savetxt("2d_distr_coord.txt", total_distr_coord)
+    np.savetxt("2d_distr_angle.txt", total_distr_angle)
 
 if __name__ == "__main__":
     beta_func = 100 #cm
