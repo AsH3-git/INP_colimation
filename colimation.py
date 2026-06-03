e6c442a8e102dfd1bfd9aa2c20ef331f703cdf31
AsH3-git <a.shulga2@g.nsu.ru>
2026-06-03 17:25:07 +0700

    Finally solved problem with Twiss-ellips distribution. Also added an updated report about this task.

--- colimation.py
+++ colimation.py
@@ -51,65 +51,40 @@ def pdf(r, phi, phi0, r0, sx=0.5, sy=1):
 sx = np.hypot(tex0, x0/L)
 sy = np.hypot(tey0, y0/L)
 
-def beam_generator(alpha_func, beta_func, epsilon):
+def beam_generator(alpha_x, alpha_y, beta_x, beta_y, eps_x, eps_y):
     """Function that generates beam distribution using np.random and other numpy tools. Should be equals to the pdf"""
-    gf = gamma_func(alpha_func, beta_func)
-    coord_disp = sqrt(epsilon * beta_func)
-    x = np.arange(-coord_disp, coord_disp, 0.5)
-    x0_hatch = -x * alpha_func/beta_func
-    x0 = -x * alpha_func/gf
+    gf_x = gamma_func(alpha_x, beta_x)
+    gf_y = gamma_func(alpha_y, beta_y)
+    coord_disp_x = sqrt(eps_x * beta_x)
+    coord_disp_y = sqrt(eps_y * beta_y)
+
     rng = np.random.default_rng()
-    weights_arr = np.array([])
-    total_distr_angle = np.array([])
-    total_distr_coord = np.array([])
-    angle_radius = sqrt(epsilon / beta_func)
-    coord_radius = sqrt(epsilon / gf)
-    for i in range(len(x)):
-        j = abs(i - len(x)/2)
-        weights_arr = np.append(weights_arr, j)
-        low_angle = min(x0_hatch[i] - angle_radius, x0_hatch[i] + angle_radius)
-        high_angle = max(x0_hatch[i] - angle_radius, x0_hatch[i] + angle_radius)
-        angle_dot = rng.uniform(low=low_angle, high=high_angle, size=100)
-        low_coord = min(x0[i] - coord_radius, x0[i] + coord_radius)
-        high_coord = max(x0[i] - coord_radius, x0[i] + coord_radius)
-        coord_dot = rng.uniform(low=low_coord, high=high_coord, size=100)
-        total_distr_angle = np.concatenate((total_distr_angle, angle_dot))
-        total_distr_coord = np.concatenate((total_distr_coord, coord_dot))
 
-    j_repl_idx = np.where(weights_arr == 0)
-    weights_arr[j_repl_idx] = 1
-    for i, element in enumerate(weights_arr):
-        if element == 1:
-            continue
-        weights_arr[i] = abs(element - (max(weights_arr) - 1)) / max(weights_arr)
-    #print(weights_arr)
-    #weights_arr1 = weights_arr[:len(weights_arr)//2]
-    #weights_arr2 = weights_arr[len(weights_arr)//2:]
-    #weights_arr = np.concatenate((weights_arr1, weights_arr2))
-    
-    """
-    total_distr_angle = total_distr_angle * weights_arr
-    print(total_distr_angle)
-    total_distr_coord = total_distr_coord * weights_arr
-    
-    angle_diff = x0_hatch - angle_radius
-    coord_diff = x0 - coord_radius
-    angle_diff = np.repeat(angle_diff, 100)
-    coord_diff = np.repeat(coord_diff, 100)
-    weights_arr = np.repeat(weights_arr, 100)
-    mask_angle = (element < (angle_diff * weights_arr))
-    mask_coord = (element < (coord_diff * weights_arr))
+    r_x = np.arange(-coord_disp_x, coord_disp_x, 0.01)
+    angle_distr_x = rng.uniform(0, 2*pi, 10000)
+    #angle_distr_x *= rng.uniform(0, 7.2)
+    r_y = np.arange(-coord_disp_y, coord_disp_y, 0.01)
+    angle_distr_y = rng.uniform(0, 2*pi, 3775)
+    #angle_distr_y = rng.normal(0, 3.3)
 
-    total_distr_angle = total_distr_angle[mask_angle]
-    total_distr_coord = total_distr_coord[mask_coord]
-    """
-    
-    np.savetxt("2d_distr_coord.txt", total_distr_coord)
-    np.savetxt("2d_distr_angle.txt", total_distr_angle)
+    x_ref = sqrt(eps_x * beta_x) * r_x * np.cos(angle_distr_x)
+    x_hatch_ref = sqrt(eps_x / beta_x) * alpha_x * r_x * np.cos(angle_distr_x) + sqrt(eps_x / beta_x) * r_x * np.sin(angle_distr_x)
+
+    y_ref = sqrt(eps_y * beta_y) * r_y * np.cos(angle_distr_y)
+    y_hatch_ref = sqrt(eps_y / beta_y) * alpha_y * r_y * np.cos(angle_distr_y) + sqrt(eps_y / beta_y) * r_y * np.sin(angle_distr_y)
+
+    np.savetxt("2d_distr_coord_x.txt", x_ref)
+    np.savetxt("2d_distr_angle_x.txt", x_hatch_ref)
+
+    np.savetxt("2d_distr_coord_y.txt", y_ref)
+    np.savetxt("2d_distr_angle_y.txt", y_hatch_ref)
 
 if __name__ == "__main__":
-    beta_func = 100 #cm
-    epsilon = 25 #nm*rad
-    alpha_func = 5 #cm
-    beam_generator(alpha_func, beta_func, epsilon)
+    beta_x = 100 #cm
+    beta_y = 15 #cm
+    eps_x = 25 #nm*rad
+    eps_y = 0.95 * eps_x #nm*rad
+    alpha_x = 5 #cm
+    alpha_y = 0.3 #cm
+    beam_generator(alpha_x, alpha_y, beta_x, beta_y, eps_x, eps_y)
 
